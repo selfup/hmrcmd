222b12f17c9790fb22ac1d418623f7777647d566
selfup <boudinot.regis@yahoo.com>
2018-07-29 21:13:14 -0500

    clean up README and scripts

--- scripts/releases.sh
+++ scripts/releases.sh
@@ -3,8 +3,8 @@ then
   rm -rf build
 fi
 
-npm run build && packr clean \
-  && GOOS=darwin GOARCH=amd64 packr build -o ./releases/darwin-hmrcmd main.go \
-  && GOOS=linux GOARCH=amd64 packr build -o ./releases/linux-hmrcmd main.go \
-  && GOOS=linux GOARCH=arm GOARM=5 packr build -o ./releases/rpi-hmrcmd main.go \
-  && GOOS=windows GOARCH=386 packr build -o ./releases/hmrcmd.exe main.go
+npm run build \
+  && packr clean && GOOS=darwin GOARCH=amd64 packr build -o ./releases/darwin-hmrcmd main.go \
+  && packr clean && GOOS=linux GOARCH=amd64 packr build -o ./releases/linux-hmrcmd main.go \
+  && packr clean && GOOS=linux GOARCH=arm GOARM=5 packr build -o ./releases/rpi-hmrcmd main.go \
+  && packr clean && GOOS=windows GOARCH=amd64 packr build -o ./releases/hmrcmd.exe main.go
