ba46cdf97cdb565596db407a831d8fadbd8173d7
selfup <boudinot.regis@yahoo.com>
2018-07-29 22:48:04 -0500

    update release script

--- scripts/releases.sh
+++ scripts/releases.sh
@@ -3,8 +3,14 @@ then
   rm -rf build
 fi
 
-npm run build \
-  && packr clean && GOOS=darwin GOARCH=amd64 packr build -o ./releases/darwin-hmrcmd main.go \
-  && packr clean && GOOS=linux GOARCH=amd64 packr build -o ./releases/linux-hmrcmd main.go \
-  && packr clean && GOOS=linux GOARCH=arm GOARM=5 packr build -o ./releases/rpi-hmrcmd main.go \
-  && packr clean && GOOS=windows GOARCH=amd64 packr build -o ./releases/hmrcmd.exe main.go
+if [[ -d releases ]]
+then
+  rm -rf releases
+fi
+
+npm run build && packr clean && packr \
+  && GOOS=darwin GOARCH=amd64 go build -o ./releases/darwin-hmrcmd main.go \
+  && GOOS=linux GOARCH=amd64 go build -o ./releases/linux-hmrcmd main.go \
+  && GOOS=linux GOARCH=arm GOARM=5 go build -o ./releases/rpi-hmrcmd main.go \
+  && GOOS=windows GOARCH=386 go build -o ./releases/hmrcmd.exe main.go \
+  && packr clean
